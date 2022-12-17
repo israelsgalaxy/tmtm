08319a5d0894a195da46b545f58c756c969b2032
Israel Galadima <izzygaladima@gmail.com>
2022-12-17 21:18:57 +0100

    Work without API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Map from './components/map'
+import points from "./points.js"
 import './styles/App.css'
 
-const token = localStorage.getItem("token") ?? "0"
+// const token = localStorage.getItem("token") ?? "0"
+const token = "12345"
 
 export default function App() {
-  let [details, setDetails] = useState(null)
+  let [details, setDetails] = useState(points)
 
-  useEffect(() => {
-    async function getPoints() {
-      let response = await fetch(`http://localhost:5000?token=${token}`)
-      let details = await response.json()
+  // useEffect(() => {
+  //   async function getPoints() {
+  //     let response = await fetch(`http://localhost:5000?token=${token}`)
+  //     let details = await response.json()
       
-      setDetails(details)
-    }
+  //     setDetails(details)
+  //   }
 
-    getPoints()
-  }, [])
+  //   getPoints()
+  // }, [])
 
   return (
     <div className="App">

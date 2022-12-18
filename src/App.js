dbd4c2a0103d958db116fb7e5818c2da0a2ae501
Victor Williams <victor.williams@stu.cu.edu.ng>
2022-12-18 05:14:10 +0100

    new changes...

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import Map from './components/map'
-import points from "./points.js"
-import './styles/App.css'
+import { useState } from "react";
+import Map from "./components/map";
+import points from "./points.js";
+import "./styles/App.css";
 
 // const token = localStorage.getItem("token") ?? "0"
-const token = "12345"
+const token = "12345";
 
 export default function App() {
-  let [details, setDetails] = useState(points)
+  let details = useState(points)[0];
 
   // useEffect(() => {
   //   async function getPoints() {
   //     let response = await fetch(`http://localhost:5000?token=${token}`)
   //     let details = await response.json()
-      
+
   //     setDetails(details)
   //   }
 
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <div className="App">
-      { details ? <Map details={details} token={token} /> : <p>Loading...</p> }
+      {details ? <Map details={details} token={token} /> : <p>Loading...</p>}
     </div>
-  )
+  );
 }

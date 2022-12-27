92757ef15a75dd9ef192877c9dec69a0854fd5b2
Victor Williams <victor.williams@stu.cu.edu.ng>
2022-12-27 15:12:16 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import Map from "./components/map";
+import Map from "./pages/map";
 import points from "./points.js";
 import "./styles/App.css";
+import Home from "./pages/Home";
+import { Link, Route, Routes } from "react-router-dom";
+import BookList from "./pages/BookList";
 
 // const token = localStorage.getItem("token") ?? "0"
 const token = "12345";
@@ -22,7 +25,25 @@ export default function App() {
 
   return (
     <div className="App">
-      {details ? <Map details={details} token={token} /> : <p>Loading...</p>}
+      {/* {details ? <Map details={details} token={token} /> : <p>Loading...</p>} */}
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/map" element={<Map details={details} token={token} />} />
+        <Route path="/" element={<Map details={details} token={token} />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/BookList.js
@@ -0,0 +1,9 @@
+const BookList = () => {
+  return (
+    <div>
+      <h1>BookList</h1>
+    </div>
+  );
+};
+
+export default BookList;
